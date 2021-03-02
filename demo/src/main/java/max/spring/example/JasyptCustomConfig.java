5355c3eb063ea0ddb0285f28b287571863de4e4c
Max Clapton <maxclapton@gmail.com>
2021-03-02 16:26:09 +0800

    password encrypted

---
+++ demo/src/main/java/max/spring/example/JasyptCustomConfig.java
@@ -0,0 +1,21 @@
+package max.spring.example;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptCustomConfig {
+
+	@Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword("springExamples");
+        config.setPoolSize(1);
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+}
